c7b5e103e0fb3f384e1bc067895b3b6fc6aa46b7
Jay Vyas Peerindex Dev Setup <jayunit100@gmail.com>
2011-08-09 11:24:21 -0400

    added project file w/ string utils dependencies.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject BioClojure "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"][commons-lang "2.3"] ])
