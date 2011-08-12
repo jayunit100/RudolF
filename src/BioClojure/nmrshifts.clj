ddc6aa96c3a207aadf11b6eb0e632b4946190a5f
Matt Fenwick <mfenwick100@gmail.com>
2011-08-12 12:18:50 -0400

    a look at average chemical shift differences

---
+++ src/BioClojure/nmrshifts.clj
@@ -0,0 +1,20 @@
+(ns BioClojure.nmrshifts
+  (:use clojure.contrib.math))
+
+(def approx-c-shifts
+  {"CA" 58,
+   "THR-CB" 68,
+   "CB" 32,
+   "SER-CB" 63,
+   "GLY-CA" 44,
+   "ALA-CB" 18,
+   "TYR-CB" 39,
+   "ILE-CD" 13,
+   "CG" 25})
+
+(defn likely-c-match
+  "get differences of input shift, average shift for various Carbon nucleus types as a sorted list of pairs"
+  [ shift ]
+  (let [diffs (for [[atom avgshift] approx-c-shifts] 	; computer difference between shift and average shift for each nucleus type
+                (list atom (abs (- shift avgshift))))]
+    (sort-by #(second %) diffs))) 	; return a sorted list (by shift difference)
