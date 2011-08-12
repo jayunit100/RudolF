a62fa603b1d87ec7986fda1fcc0f54cf8f595e62
jayunit100 <jayunit100@gmail.com>
2011-08-12 17:41:38 -0300

    Added comments to matts function that will help beginners to recognize some corner cases with mapping functions,  string data types, and the "str" function.

--- src/BioClojure/nmr.clj
+++ src/BioClojure/nmr.clj
@@ -25,7 +25,10 @@
 	"W" "TRP"
 	})
 
-
+;;This demonstrates two important distinction in clojure 
+;;(1) Strings are "Sequences" : When we send "ABCDEFGH" as an argument to a "map" function, each character is individually sent to the map's function (in this case, "convert").
+;;(2) In order to find a char in the Map, clojure must "convert" the char into a string. So there are elements of 
+;;strong typing in Clojure.  
 (defn convert-sequence-123
   "Converts a string into a list of 3-letter amino acid names.  Needs refactoring:  assumes capital letters; if letter not found, mapping function returns nil; accepts string instead of list"
   [ string ]
