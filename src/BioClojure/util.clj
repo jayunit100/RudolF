b7feffaa4b5a38f295ea373db0bdc15e539c74c4
Jay Vyas Peerindex Dev Setup <jayunit100@gmail.com>
2011-08-14 19:45:31 -0400

    util modification, added a simple method,

--- src/BioClojure/util.clj
+++ src/BioClojure/util.clj
@@ -10,41 +10,16 @@
 (defn hasSubstring "has" [ main b ]
   (map #(.indexOf main %) b     ))
 
-(defn transform-entry
-  "Transform a meta string into a map of metadata about a DNA entry."
-  [entry]
-  (let [meta-props (seq (-> (subs entry 1) ;; take off the leading ">"
-                            (.split " ")))
-        name (first meta-props)
-        props (drop 1 meta-props)]
-    (assoc (into {} (map #(vec (.split % "=")) props)) "name" name)))
+(import '(java.io File))
+(defn kind [filename]
+  ;;create a new file by calling the File(filename) constructor)
+  (let [f (File. filename)]
+    (cond 
+      (.isFile f) "file"
+      (.isDirectory f) "directory"
+      (.exists f) "other"
+      :else "non-existent")))
 
-(defn read-fasta-file
-  "Read a FASTA file, transforming it into a map with :meta and :dna keys."
-  [filename]
-  (let [text (.trim (slurp filename))
-        entries (-> text (.replaceAll "\r" "") (.split "\n\n") seq)]
-    (map #(let [[m dna] (.split % "\n")]
-            {:meta (transform-entry m) :dna dna})
-         entries)))
 
-(defn read-and-print
-  "Read a FASTA file and print it."
-  [filename]
-  (pprint (read-fasta-file filename)))
 
-;; From the project directory:
-;; lein repl
-;; (use 'BioClojure.core)
-;; (read-and-print "data/sample.fa")
-
-;; 1 Identify motifs (M1 - Mx) present in DNA and their order
-;; Confirm initiator motif and terminator motif and positions
-;; Confirm proper insertion into two restriction sites.
-;; Flag any incorrect assemblies and sequences with N positions
-;; Report total number of minimotifs in clone
-;; Generate a graph of overall structure, show linker regions that
-;; glue motifs together and alignm
-;; of motifs to sequences with labels.
-;; 7. Report any 1 nucleotide mutations in DNA
 
