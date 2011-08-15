ba1642de1bfdf0cf160aa6211304ed4fd41615e4
Matt Fenwick <mfenwick100@gmail.com>
2011-08-15 17:53:36 -0400

    added beginnings of library for generating NMR non-uniform sample schedules

---
+++ src/BioClojure/sampleSchedule.clj
@@ -0,0 +1,30 @@
+(ns BioClojure.sampleSchedule)
+
+
+(defstruct schedule :points :num-dimensions)
+
+
+
+(defn make-schedule
+  [ point-map num-dimensions ]
+  (let [points (keys point-map)
+        is-dims-correct? (fn [pt] 
+                           (= num-dimensions 
+                             (count (pt :coordinates)) ; is the number of coordinates correct?
+                             (count (pt :quadrature))))] ; is the number of quad things correct?
+    (if (every? is-dims-correct? points)
+      (struct schedule point-map num-dimensions)
+      "oops -- there was a problem")))
+
+
+(defn uniformly-sampled-grid
+  [ xlow xhigh ylow yhigh ]
+  (for [x (range xlow xhigh) 
+        y (range ylow yhigh)]
+    {:coordinates (list x y) :quadrature (list \R \R)})) ;; I need this in a map ....
+
+
+(def example 
+  (make-schedule
+    (into {} (map (fn [x] [x 1]) mylist)) ; convert list to map (with default values of 1)
+    2))
