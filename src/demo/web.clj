a27bc4d4bcc1a9263bc8d3c20ae68b4d300000a8
Jay Vyas Peerindex Dev Setup <jayunit100@gmail.com>
2011-08-15 00:36:11 -0400

    web stuff

--- src/demo/web.clj
+++ src/demo/web.clj
@@ -4,7 +4,7 @@
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello, world"})
+   :body "Welcome Mini-ClotifMiner : The Flagship App of the BioClojure Framework "})
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
