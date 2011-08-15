1dfe15460ab439ec4242f356487241317f2127fc
Jay Vyas Peerindex Dev Setup <jayunit100@gmail.com>
2011-08-15 01:21:34 -0400

    Changed web file to generate dna sequences

--- src/demo/web.clj
+++ src/demo/web.clj
@@ -1,10 +1,11 @@
-(ns demo.web
-  (:use ring.adapter.jetty))
+(ns demo.web (:use [ring.adapter.jetty] [BioClojure.core] ))
 
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Welcome Mini-ClotifMiner : The Flagship App of the BioClojure Framework "})
+   :body (str ["Welcome Mini-ClotifMiner : The Flagship App of the BioClojure Framework " (rand-motif-str (rand-int 15)) ] )   
+ }
+)
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
