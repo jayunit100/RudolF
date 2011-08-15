b5ebe16136d53114d6c06477a7bd0cb6665d8077
mattfenwick <mfenwick@student.uchc.edu>
2011-08-15 13:34:48 -0300

    edited convert-sequence-123 explanation

--- src/BioClojure/nmr.clj
+++ src/BioClojure/nmr.clj
@@ -26,9 +26,11 @@
 	})
 
 ;;This demonstrates two important distinction in clojure 
-;;(1) Strings are "Sequences" : When we send "ABCDEFGH" as an argument to a "map" function, each character is individually sent to the map's function (in this case, "convert").
-;;(2) In order to find a char in the Map, clojure must "convert" the char into a string. So there are elements of 
-;;strong typing in Clojure.  
+;;(1) Strings are "Sequences" : When we send "ABCDEFGH" as an argument to a "map" function, 
+;;     each character is individually sent to the map's function (in this case, "convert").
+;;(2) In order to find a key (which is a string in this case) in the Map, 
+;;     the char must be converted into a string
+;;(3) Clojure is dynamically, strongly typed -- source:  http://www.citerus.se/post/220609-from-java-to-clojure
 (defn convert-sequence-123
   "Converts a string into a list of 3-letter amino acid names.  Needs refactoring:  assumes capital letters; if letter not found, mapping function returns nil; accepts string instead of list"
   [ string ]
