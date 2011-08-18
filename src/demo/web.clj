bc9a22e3f32f6a4a44fff26ff3ba72c66c0113e0
Jay Vyas Peerindex Dev Setup <jayunit100@gmail.com>
2011-08-18 00:14:13 -0400

    web is now pretty print enabled.

--- src/demo/web.clj
+++ src/demo/web.clj
@@ -5,17 +5,25 @@
         random_aa (rand-aa-str (rand-int 40))
         translated_aa (aa-to-3-letter random_aa)]
   
-  ["rand dna = " random_motif "; a  rand protein = " random_aa " which is  translated to " translated_aa]
+   ["rand dna = " random_motif "; a  rand protein = " random_aa " which is  translated to " translated_aa]
   )
 )
 
+(defn demoPretty []
+	(interpose "<BR> " (demo))
+)
+
+ 
+
 (defn app [req]
   {:status 200
-   :headers {"Content-Type" "text/plain"}
+   :headers {"Content-Type" "text/html"}
    :body (str 
-         ["Welcome Mini-ClotifMiner : The Flagship App of the BioClojure Framework " 
-         (demo) 
-         ] 
+         ["Welcome Mini-ClotifMiner : The Flagship App of the BioClojure Framework <BR></BR> demo:" 
+           (demo) 
+          "<H1>A Prettier Version : in one line of code, using ' interpose ' demoPretty:</H1>"
+           (demoPretty)
+	  ] 
 	 )
    }
   )
