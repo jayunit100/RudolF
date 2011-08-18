e303d5877bfcd4dc9152ed8c162c7c9010991799
mattfenwick <mfenwick@student.uchc.edu>
2011-08-18 15:11:08 -0300

    adjusted indentation and trailing parentheses according to clojure conventions given on stackoverflow.com

--- src/demo/web.clj
+++ src/demo/web.clj
@@ -1,17 +1,14 @@
-(ns demo.web (:use [ring.adapter.jetty] [BioClojure.seqUtils] ))
+(ns demo.web 
+  (:use [ring.adapter.jetty] [BioClojure.seqUtils] ))
 
 (defn demo []
   (let [random_motif (apply str (rand-motif-str (rand-int 30)))
         random_aa (rand-aa-str (rand-int 40))
         translated_aa (aa-to-3-letter random_aa)]
-  
-   ["rand dna = " random_motif "; a  rand protein = " random_aa " which is  translated to " translated_aa]
-  )
-)
+   ["rand dna = " random_motif "; a  rand protein = " random_aa " which is  translated to " translated_aa]))
 
 (defn demoPretty []
-	(interpose "<BR> " (demo))
-)
+  (interpose "<BR> " (demo)))
 
  
 
@@ -19,14 +16,10 @@
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (str 
-         ["Welcome Mini-ClotifMiner : The Flagship App of the BioClojure Framework <BR></BR> demo:" 
+           ["Welcome Mini-ClotifMiner : The Flagship App of the BioClojure Framework <BR></BR> demo:" 
            (demo) 
-          "<H1>A Prettier Version : in one line of code, using ' interpose ' demoPretty:</H1>"
-           (demoPretty)
-	  ] 
-	 )
-   }
-  )
+           "<H1>A Prettier Version : in one line of code, using ' interpose ' demoPretty:</H1>"
+           (demoPretty)])})
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
