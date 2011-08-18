0a23935ac8b2263327e8786da70f63bd019b1ceb
Lee Hinman <lee@writequit.org>
2011-08-17 23:25:31 -0400

    fix weird unprintable characters in web.clj

--- src/demo/web.clj
+++ src/demo/web.clj
@@ -1,18 +1,16 @@
 (ns demo.web (:use [ring.adapter.jetty] [BioClojure.seqUtils] ))
 
-(defn demo[]
-
-	(let [random_motif (str (rand-motif-str (rand-int 30)))
-       random_aa (rand-aa-str (rand-int 10))
-       translated_aa (aa-to-3-letter "ASADD" )])  
-)
+(defn demo []
+  (let [random_motif (apply str (rand-motif-str (rand-int 30)))
+        random_aa (rand-aa-str (rand-int 10))
+        translated_aa (aa-to-3-letter "ASADD")]))
 
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body (str ["Welcome Mini-ClotifMiner : The Flagship App of the BioClojure Framework " (rand-motif-str (rand-int 15)) ] )   
- }
-)
+   :body (str ["Welcome Mini-ClotifMiner : The Flagship App of the BioClojure Framework " (rand-motif-str (rand-int 15)) ] )
+   }
+  )
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
