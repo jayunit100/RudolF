4b107d1b111b07cf67a557836cf25c118c47b168
Lee Hinman <lee@writequit.org>
2011-08-31 10:08:01 -0600

    commas are optional

--- src/BioClojure/seqUtils.clj
+++ src/BioClojure/seqUtils.clj
@@ -80,26 +80,26 @@
 
 ;;Define the 1-3 letter mapping.
 (def one-to-three
-  {"R" "ARG",
-   "H" "HIS",
-   "K" "LYS",
-   "D" "ASP",
-   "E" "GLU",
-   "S" "SER",
-   "T" "THR",
-   "N" "ASN",
-   "Q" "GLN",
-   "C" "CYS",
-   "U" "SEC", ;; interesting amino acid
-   "G" "GLY",
-   "P" "PRO",
-   "A" "ALA",
-   "V" "VAL",
-   "I" "ILE",
-   "L" "LEU",
-   "M" "MET",
-   "F" "PHE",
-   "Y" "TYR",
+  {"R" "ARG"
+   "H" "HIS"
+   "K" "LYS"
+   "D" "ASP"
+   "E" "GLU"
+   "S" "SER"
+   "T" "THR"
+   "N" "ASN"
+   "Q" "GLN"
+   "C" "CYS"
+   "U" "SEC" ;; interesting amino acid
+   "G" "GLY"
+   "P" "PRO"
+   "A" "ALA"
+   "V" "VAL"
+   "I" "ILE"
+   "L" "LEU"
+   "M" "MET"
+   "F" "PHE"
+   "Y" "TYR"
    "W" "TRP"})
 
 ;; This demonstrates two important distinctions in clojure (1) Strings
