4fce891495b9ed091c0d40760b161b4361945525
Jay Vyas Peerindex Dev Setup <jayunit100@gmail.com>
2011-09-03 01:03:07 -0400

    more examples and stub code in clomol.clj

--- src/BioClojure/clomol.clj
+++ src/BioClojure/clomol.clj
@@ -1,6 +1,7 @@
-(ns BioClojure.clomol)
-
-(use 'BioClojure.vennnmr)
+(ns BioClojure.clomol
+  (:use [clojure.contrib.string :only (replace-by)])
+  (:use clojure.contrib.generic.functor)
+  )
 
 ;;Here is a starting point - it will launch biojava jmol gui. 
 ;;INPUT a biojava Structure object (org.biojava.bio.structure.Structure)
@@ -13,10 +14,26 @@
 (defn getStructure [pdbid]
  (.getStructure (new org.biojava.bio.structure.align.util.AtomCache "/tmp" true) pdbid))
 
+;;Creates a string that colors an amino acid
+(defn colorAA [index]
+ (.replaceAll "select index; color green" "index" (str index)) ) 
+
 ;;Launch a viewer that views trp represor
+;;And then colors it blue, with spacefill. 
+;;This is a good example of how to use object API's in clojure, via the let function.
 (defn ex1 []
- (launchGui(getStructure "1WRP"))
-)
+  (let [panel (new org.biojava.bio.structure.gui.BiojavaJmol)] 
+       (.setStructure panel (getStructure "1WRP")) 
+       (.evalString panel "select *; spacefill 200; wireframe off; backbone 0.4; color chain")))
 
 
+;;returns a map of amino acid numbers -> rgb colors , to use venn nmr
+(defn aaColors [] 
+  ([1 2 3]) )
 
+;;incomplete method to map color statements 
+;;over all residues.
+(defn ex2 []
+  (let [panel (new org.biojava.bio.structure.gui.BiojavaJmol)]
+       (.setStructure panel (getStructure "1WRP"))
+       (map #(colorAA %) (aaColors)) ))
