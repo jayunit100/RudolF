23de2348c467d21dabf0e6a09d5fb0cb0c47a2e6
mattfenwick <mfenwick@student.uchc.edu>
2011-09-06 11:47:08 -0600

    adjusted layout, moved comments into function docs, changed no-args function to to name binding

--- src/BioClojure/clomol.clj
+++ src/BioClojure/clomol.clj
@@ -1,39 +1,46 @@
 (ns BioClojure.clomol
   (:use [clojure.contrib.string :only (replace-by)])
-  (:use clojure.contrib.generic.functor)
-  )
+  (:use clojure.contrib.generic.functor))
 
-;;Here is a starting point - it will launch biojava jmol gui. 
-;;INPUT a biojava Structure object (org.biojava.bio.structure.Structure)
-(defn launchGui [s]
- (.setStructure (new org.biojava.bio.structure.gui.BiojavaJmol) s)
-)
-;;Get a structure From the pdb 
-;;INPUT a pdb id
-;;OUPUT is a org.biojava.bio.structure.Structure object.
-(defn getStructure [pdbid]
- (.getStructure (new org.biojava.bio.structure.align.util.AtomCache "/tmp" true) pdbid))
+(defn launchGui 
+  "Here is a starting point - it will launch biojava jmol gui. 
+   INPUT a biojava Structure object (org.biojava.bio.structure.Structure)"
+  [s]
+  (.setStructure (new org.biojava.bio.structure.gui.BiojavaJmol) s))
 
-;;Creates a string that colors an amino acid
-(defn colorAA [index]
- (.replaceAll "select index; color green" "index" (str index)) ) 
+(defn getStructure 
+  "Get a structure From the pdb 
+    INPUT a pdb id
+    OUPUT is a org.biojava.bio.structure.Structure object."
+  [pdbid]
+  (.getStructure (new org.biojava.bio.structure.align.util.AtomCache "/tmp" true) pdbid))
 
-;;Launch a viewer that views trp represor
-;;And then colors it blue, with spacefill. 
+(defn colorAA 
+  "Creates a string that colors an amino acid"
+  [index]
+  (.replaceAll "select index; color green" "index" (str index)) ) 
+
 ;;This is a good example of how to use object API's in clojure, via the let function.
-(defn ex1 []
+;; @Jay -- 'let' is a special form to bind values to lexically-scoped names, not a function
+;;    what is the return value intended to be?  panel?  what is the return value of .evalString?
+;; can this no-arg procedure also be refactored as a (def ...) binding?
+(defn ex1 
+  "Launch a viewer that views trp represor. And then colors it blue, with spacefill. "
+  []
   (let [panel (new org.biojava.bio.structure.gui.BiojavaJmol)] 
        (.setStructure panel (getStructure "1WRP")) 
        (.evalString panel "select *; spacefill 200; wireframe off; backbone 0.4; color chain")))
 
 
-;;returns a map of amino acid numbers -> rgb colors , to use venn nmr
-(defn aaColors [] 
-  ([1 2 3]) )
+;; this is (?supposed?) to be a map of amino acid numbers -> rgb colors , to use venn nmr
+;; refactored from no-args function to a name binding
+(def aaColors 
+  ([1 2 3]) ) ;; @Jay -- is this supposed to be a map?  should it be [1 2 3] -- without the parentheses?
 
 ;;incomplete method to map color statements 
 ;;over all residues.
 (defn ex2 []
   (let [panel (new org.biojava.bio.structure.gui.BiojavaJmol)]
        (.setStructure panel (getStructure "1WRP"))
-       (map #(colorAA %) (aaColors)) ))
+       (map colorAA aaColors)))
+
