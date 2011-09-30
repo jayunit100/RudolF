88e1937d7ac80f5e5e039b69782f696c9118ab5e
Jay Vyas Peerindex Dev Setup <jayunit100@gmail.com>
2011-09-30 01:48:42 -0400

    lein - incanter

--- xl_clojure/project.clj
+++ xl_clojure/project.clj
@@ -1,3 +1,3 @@
 (defproject xl_clojure "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[incanter "1.2.3-SNAPSHOT"][org.clojure/clojure "1.2.1"]])
