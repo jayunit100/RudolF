69a743ebc03bd7c54f7d7bf1078c36bbd7804413
jay <jayunit100@gmail.com>
2011-10-14 17:44:48 -0400

    Added unit test file in correct namespace

---
+++ bioclojure/test/tests.clj
@@ -0,0 +1,14 @@
+(ns tests
+    (:use clojure.test))
+
+(defn myfixture [block] 
+    (do 
+        (println "before test")
+        (block)
+        (println "after test")))
+
+(use-fixtures :each myfixture)
+
+(deftest mytest []
+    (is (= 2 (+ 1 1))))
+
