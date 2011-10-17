c788fa33d2bced9dd9d88737080502313d86df31
jay <jayunit100@gmail.com>
2011-10-17 18:12:14 -0400

    test

--- bioclojure/test/tests.clj
+++ bioclojure/test/tests.clj
@@ -3,10 +3,12 @@
     
 
 ;;Callable functions, make sure there are no namespace errors
+;;This function returns true or nil.
 (defn callable? 
   [s] 
   (let [obj (try (eval (symbol s)) (catch Exception e))]
   (and obj (fn? obj))))
+
   
 ;;A Fixture is a scaffold for each test.  these statements are executed before/after the tests.
 (defn myfixture [block] 
@@ -15,7 +17,6 @@
         (println (resolve (symbol "parse-bmrb-stats")))
         (block)
         (println "   ")))
-
 
 ;;Fixtures are a clojure construct for wrapping tests. 
 (use-fixtures :each myfixture) 
@@ -26,4 +27,4 @@
 
 ;;Tests that we can see matts code 
 (deftest testNmrReader [] 
-		(is (= true  (callable? "parse-bmrb-stats")   )) )
+		(is (= true  (callable? "parse-bmrb-stats") )) )
