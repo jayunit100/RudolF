3bb614787202d785bb9e4e66bd57e8bf9ba9e37e
Matt Fenwick <mfenwick100@gmail.com>
2011-10-17 16:08:55 -0400

    added first mapper to convert protein data model to graphable data

---
+++ bioclojure/src/ConnjurRV/statmapper.clj
@@ -0,0 +1,9 @@
+(ns ConnjurRV.statmapper
+  (:use clojure.contrib.generic.functor))
+
+
+(defn protein-to-num-atoms-per-residue
+  "Protein -> Map index Integer
+  "
+  [prot]
+  (fmap (fn [res] (count (res :atoms))) prot))
