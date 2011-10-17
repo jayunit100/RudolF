9e28b64606a8934e7822c7c3495a90319c07ac23
Colbert Sesanker <sesanker0@gmail.com>
2011-10-17 15:57:24 -0300

    added incanter

--- bioclojure/project.clj
+++ bioclojure/project.clj
@@ -7,5 +7,6 @@
                  [jmol/jmol "12.0.49"]
 		 [org.biojava/biojava3-core "3.0"]
 		 [org.biojava/biojava3-structure "3.0"]
-		 [org.biojava/biojava3-structure-gui "3.0"]]
+		 [org.biojava/biojava3-structure-gui "3.0"]
+		 [incanter "1.2.3-SNAPSHOT"]]
   :repositories [["biojava-maven-repo" "http://www.biojava.org/download/maven/"]])
