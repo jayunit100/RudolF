371cee7a01973c50dc66d7507e0566d3d8d510c7
Matt Fenwick <mfenwick100@gmail.com>
2011-10-18 09:15:39 -0400

    removed examples from statmapper; cleaned up imports, added comments, moved example to a function in dataplot

--- bioclojure/src/ConnjurRV/dataplot.clj
+++ bioclojure/src/ConnjurRV/dataplot.clj
@@ -1,25 +1,21 @@
-(ns ConnjurRV.statmapper
+(ns ConnjurRV.dataplot
   (:use clojure.contrib.generic.functor)
-  (:use [ConnjurRV.readcyana :only (make-protein-from-files)]))
-
-(use '(incanter core stats charts io))
-
-(defn protein-to-num-atoms-per-residue
-  "Protein -> Map index Integer
-  "
-  [prot]
-  (fmap (fn [res] (count (res :atoms))) prot))
-
+  (:use [ConnjurRV.readcyana :only (make-protein-from-files)])
+  (:use [ConnjurRV.statmapper :only (protein-to-num-atoms-per-residue)]);)
+  (:use [incanter.charts :only (histogram)])
+  (:use [incanter.core :only (view)]))
 
 (def example-stats 
   (protein-to-num-atoms-per-residue 
    (make-protein-from-files "data/connjur.seq" "data/connjur.prot")))
 
 (defn hist 
-  "test function"
-  [m]
-  (histogram (vals m)))
+  "test function -- displays histogram.  
+   todos:  figure out how to control the x-axis label
+	figure out how to change the x-values, or how they're generated"
+  []
+  (view
+   (histogram (vals example-stats))))
 
-(view (hist example-stats))
-  		
+
 
--- bioclojure/src/ConnjurRV/statmapper.clj
+++ bioclojure/src/ConnjurRV/statmapper.clj
@@ -1,8 +1,5 @@
 (ns ConnjurRV.statmapper
-  (:use clojure.contrib.generic.functor)
-  (:use [ConnjurRV.readcyana :only (make-protein-from-files)]))
-
-(use '(incanter core stats charts io))
+  (:use clojure.contrib.generic.functor))
 
 (defn protein-to-num-atoms-per-residue
   "Protein -> Map index Integer
@@ -10,16 +7,5 @@
   [prot]
   (fmap (fn [res] (count (res :atoms))) prot))
 
-
-(def example-stats 
-  (protein-to-num-atoms-per-residue 
-   (make-protein-from-files "data/connjur.seq" "data/connjur.prot")))
-
-(defn hist 
-  "test function"
-  [m]
-  (histogram (vals m)))
-
-(view (hist example-stats))
   		
 
