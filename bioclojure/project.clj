64da5d2529b3efebaf55b3972394d8dcfec81805
Colbert Sesanker <sesanker0@gmail.com>
2011-10-24 10:42:39 -0300

    Add lein eclipse for building with eclipse

--- bioclojure/project.clj
+++ bioclojure/project.clj
@@ -8,5 +8,6 @@
 		 [org.biojava/biojava3-core "3.0"]
 		 [org.biojava/biojava3-structure "3.0"]
 		 [org.biojava/biojava3-structure-gui "3.0"]
-		 [incanter "1.2.3-SNAPSHOT"]]
+		 [incanter "1.2.3-SNAPSHOT"]
+		 [lein-eclipse "1.0.0"]]
   :repositories [["biojava-maven-repo" "http://www.biojava.org/download/maven/"]])
