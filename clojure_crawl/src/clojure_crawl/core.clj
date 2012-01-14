30d92f5b13c7d0046821fb04f3280a0786e8c268
Jay Vyas <jayunit100@Users-MacBook-Pro-17.local>
2012-01-13 19:26:17 -0500

    clojurecrawl init

---
+++ clojure_crawl/src/clojure_crawl/core.clj
@@ -0,0 +1,1 @@
+(ns clojure_crawl.core)
---
+++ clojure_crawl/test/clojure_crawl/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure_crawl.test.core
+  (:use [clojure_crawl.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
