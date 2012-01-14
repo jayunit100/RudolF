3a771113fb6550592409659a6705b02a65ed1321
Jay Vyas <jayunit100@Users-MacBook-Pro-17.local>
2012-01-13 20:28:21 -0500

    http requests and json maps, clojure - crawling examples

--- clojure_crawl/project.clj
+++ clojure_crawl/project.clj
@@ -1,3 +1,9 @@
-(defproject clojure_crawl "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+(defproject clojure-crawl "1.0.0-SNAPSHOT" 
+            :description "FIXME: write description" 
+            :dependencies [[org.clojure/clojure "1.2.1"] 
+                           [commons-lang "2.3"] 
+                           [ring/ring-jetty-adapter "0.3.9"] 
+                           [org.clojure/clojure-contrib "1.2.0"] 
+                           [clj-http "0.1.3"]]) 
+
+
--- clojure_crawl/src/clojure_crawl/core.clj
+++ clojure_crawl/src/clojure_crawl/core.clj
@@ -1,1 +1,25 @@
 (ns clojure_crawl.core)
+(require '[clj-http.client :as client])
+(use 'clojure.contrib.json)
+
+;;This script exemplifies the basics of 
+;;web requests and json parsing, in clojure.
+;;Example output : 
+;;user=> (getJsonFieldNames "https://graph.facebook.com/peerindex")
+;;  (:can_post :company_overview :link :mission :name :likes :products :username :talking_about_count :founded :id :website :picture :category)
+
+
+;;goes to a url and gets contents.
+(defn getUrl [url] 
+  (client/get url {:accept :json}) 
+  )
+
+;;goes to a url, gets contents, and returns the body only. 
+(defn getBod [url]
+ ((read-json (:body url))))
+
+;;goes to a url, gets contents, pasrses them as a json into a map, and gets the json keys only.
+(defn getJsonFieldNames [url]
+ (keys (read-json (:body (client/get url))) )
+ )
+
