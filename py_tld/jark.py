ea59b1537246d37161c8f0cd1cd6c504e49286f7
Matt Fenwick <mfenwick100@gmail.com>
2012-01-30 12:28:05 -0500

    tidy up code -- remove ;s, add spacing, use list literals and docstrings

--- py_tld/jark.py
+++ py_tld/jark.py
@@ -7,39 +7,39 @@ import urllib.request
 from string import whitespace
 from collections import Counter
 
-#input : a csv file
-#output :  returns a list of all urls
+
 def source_urls():
+    '''input : a csv file
+       output :  returns a list of all urls'''
     data = open('./file.csv','r') # enter the file name for file
-    spamReader=csv.reader(data , delimiter='\t') # take col
-    myUrls = list()
-    myUrls = make_t_url_list(spamReader,23) #
+    spamReader = csv.reader(data , delimiter='\t') # take col
+    myUrls = make_t_url_list(spamReader, 23) #
     normalizeAllUrls(myUrls)
     
 
-#input : A list of space/tab separated urls
-#output : A map of normalized urls
 def normalizeAllUrls(myUrls):
-    #myUrl is a string of many urls http://www.a.com http://    www.b.com ....
+    '''input : A list of space/tab separated urls
+    output : A map of normalized urls
+    myUrl is a string of many urls http://www.a.com http://    www.b.com ....'''
     c = Counter()
     for urlList in myUrls:
         for url in urlList.split(" "):
-            n = normalizeURL(url);
+            n = normalizeURL(url)
             c[n] += 1
-    return c;
+    return c
 
-#input : a single URL
-#output : a normalize URL 
 def normalizeURL(url):
+    '''input : a single URL
+    output : a normalized URL'''
     call = "http://tldextract.appspot.com/api/extract?url=" + url    
     normalizeUrl = urllib.request.urlopen(call).read()
-    print (normalizeUrl);
+    print (normalizeUrl)
     return normalizeUrl
 
-#input : A reader (presumabley of a large, multi column csv)
-#output : a list of urls extracted from column n of csv.
 def make_t_url_list(t_reader,n):
-    tUrls = list()
+    '''input : A reader (presumabley of a large, multi column csv)
+    output : a list of urls extracted from column n of csv.'''
+    tUrls = []
     for row in t_reader:
         if len(row) > n:
             tUrls.append(row[n-1]) ## not general needs fixing
