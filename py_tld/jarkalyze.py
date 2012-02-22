ec6908553230a727bedbe200137cc02c213cff3d
Matt Fenwick <mfenwick@student.uchc.edu>
2012-02-21 18:09:51 -0600

    add logging and unit tests to jark-erator

--- py_tld/jarkalyze.py
+++ py_tld/jarkalyze.py
@@ -1,6 +1,11 @@
 import csv
 import urllib2
 import json
+import logging
+import unittest
+
+
+mylogger = logging.getLogger("jarkalyze")
 
 
 #####################################################################
@@ -8,21 +13,22 @@ import json
 def parseJark(path):
     '''in: path to a jarkfile
        out: jarkmodel'''
-    data = open(path, 'r') # enter the file name for file
+    mylogger.debug("opening jarkfile " + str(path))
+    data = open(path, 'r')
+    mylogger.debug("reading jarkfile " + str(path))
     spamReader = csv.reader(data, delimiter='\t') # take col
-    return [line for line in spamReader]
+    lines = [line for line in spamReader]
+    mylogger.debug("found %d lines in jarkfile" % len(lines))
+    return lines
 
 
 def extractBaseUrls(jarkModel, columnno):
     '''in: jarkmodel
-       out: a list of base urls from column number <columnno> of each row.  skips rows that are not long enough'''
+       out: a list of base urls from column number <columnno> of each row.  
+            skips rows that are not long enough'''
     possiblyNestedList = [row[columnno - 1].split(" ") for row in jarkModel if len(row) > columnno]
     return reduce(lambda x,y: x + y, possiblyNestedList) # flatten the list
 
-
-def readJark():
-    '''this is a junk function; it should be placed in a separate file'''
-    return extractBaseUrls(parseJark('samplecsv.txt'), 23)
 
 #####################################################################
 
@@ -37,7 +43,11 @@ def makeTldUrl(baseUrl):
 def getWebPage(url):
     '''input: a url
        output: the contents found at that url'''
-    return urllib2.urlopen(url).read()
+    mylogger.debug("trying to grab url <%s>" % url)
+    openedurl = urllib2.urlopen(url)
+    mylogger.debug("response object: " + str(dir(openedurl)))
+    text = openedurl.read()
+    return text
 
 
 def parseTldResponse(string):
@@ -52,10 +62,41 @@ def sendTldUrls(baseUrls):
     return tldResponses
 
 #####################################################################
+# tests
 
+class JarkfileTest(unittest.TestCase):
+    def setUp(self):
+        pass
+    def testReadjarkfile(self):
+        jm = parseJark('samplecsv.txt')
+        self.assertEqual(len(jm), 3, "number of lines in jarkfile")
+    def testExtractBaseUrls(self):
+        pass
+    
+class TldTest(unittest.TestCase):
+    def setUp(self):
+        pass
+    def testMakeTldUrl(self):
+        myurl = "http://whatever.com"
+        tld = "http://tldextract.appspot.com/api/extract?url="
+        self.assertEqual(tld + myurl, makeTldUrl(myurl), "tld url maker")
+    def testGetWebPage(self):
+        content = getWebPage("http://www.google.com")
+        self.assertTrue(len(content) > 5000, "page content")
+    def testParseTldResponse(self):
+        response = '{"domain": "abc", "subdomain": "", "tld": ""}'
+        parsed = parseTldResponse(response)
+        self.assertTrue(parsed.has_key("domain"))
+        self.assertTrue(parsed.has_key("subdomain"))
+        self.assertTrue(parsed.has_key("tld"))
+    def testSendTldUrls(self):
+        pass
+    def testBadTldUrl(self):
+        pass
 
-if __name__ == "__main__":
-    # TODO: put this junk somewhere else
-    urls = j.readJark()
-    j.sendTldUrls(urls)
-
+def runTestSuite():
+    suite1 = unittest.TestLoader().loadTestsFromTestCase(JarkfileTest)
+    suite2 = unittest.TestLoader().loadTestsFromTestCase(TldTest)
+    
+    mySuite = unittest.TestSuite([suite1, suite2])
+    unittest.TextTestRunner(verbosity=2).run(mySuite)
---
+++ py_tld/jarksecute.py
@@ -0,0 +1,22 @@
+import jarkalyze as jk
+import logging
+
+LOG_FILENAME = "logging.txt"
+logging.basicConfig(filename = LOG_FILENAME, level = logging.DEBUG, filemode = 'w')
+
+
+def readJark():
+    '''this is a junk function; it should be placed in a separate file'''
+    testpath = 'samplecsv.txt'
+    jarkModel = jk.parseJark(testpath)
+    return jk.extractBaseUrls(jarkModel, 23)
+
+
+if __name__ == "__main__":
+    # TODO: put this junk somewhere else
+    logging.debug("reading jarkfile")
+    urls = readJark()
+    logging.debug("sending urls to tld extractor")
+    jk.sendTldUrls(urls)
+else:
+    raise Exception("this file should only be run as a top-level script")
