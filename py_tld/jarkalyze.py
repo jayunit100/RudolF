16bd5d78672921a69573e8e6ef72e12fb464946a
Matt Fenwick <mfenwick100@gmail.com>
2012-02-22 10:05:46 -0500

    move unit tests out of jarkalyze; move http error handling up; remove print statements

--- py_tld/jarkalyze.py
+++ py_tld/jarkalyze.py
@@ -2,7 +2,6 @@ import csv
 import urllib2
 import json
 import logging
-import unittest
 
 
 mylogger = logging.getLogger("jarkalyze")
@@ -36,27 +35,19 @@ def makeTldUrl(baseUrl):
     '''input : a single base Url
        output : a Url for Tld consumption'''
     # TODO: check whether "baseUrl" needs to be encoded/escaped
-    fullUrl = "http://tldextract.appspot.com/api/extract?url=" + baseUrl   
-    print fullUrl;
+    fullUrl = "http://tldextract.appspot.com/api/extract?url=" + baseUrl
+    mylogger.debug("built tld url " + fullUrl) 
     return fullUrl
-
-def validateURL(url):
-    if "http" in url:
-        return True;
-    else:
-        return False;
 
 
 def getWebPage(url):
     '''input: a url
-       output: the contents found at that url'''
-    print "trying to grab url <%s>" % url 
-    if validateURL(url): 
-        openedurl = urllib2.urlopen(url)
-    else:
-        print "\n ERROR : ODD URL - skipping extraction <%s>  " % url ;
-    #print "response object: " + str(dir(openedurl))
+       output: the contents found at that url
+       or an exception if the server responds with an error'''
+    mylogger.debug("trying to grab url <%s>" % url)
+    openedurl = urllib2.urlopen(url)
     text = openedurl.read()
+    mylogger.debug("successfully grabbed url and read response")
     return text
 
 
@@ -66,47 +57,14 @@ def parseTldResponse(string):
 
 def sendTldUrls(baseUrls):
     '''input:  iterable of urls for tld consumption
-       output: list of dictionaries of tld-ified urls'''
+       output: list of dictionaries of tld-ified urls
+       if any of the tld urls cause an exception, that url is skipped and the error is simply logged'''
     tldUrls = [makeTldUrl(baseUrl) for baseUrl in baseUrls]
-    tldResponses = [parseTldResponse(getWebPage(tldUrl)) for tldUrl in tldUrls]
+    tldResponses = []
+    for tldUrl in tldUrls:
+        try:
+            tldResponses.append(parseTldResponse(getWebPage(tldUrl)))
+        except urllib2.HTTPError, e:
+            mylogger.error("tld url <%s> failed with message <%s>" % (tldUrl, e.msg))
     return tldResponses
 
-#####################################################################
-# tests
-
-class JarkfileTest(unittest.TestCase):
-    def setUp(self):
-        pass
-    def testReadjarkfile(self):
-        jm = parseJark('samplecsv.txt')
-        self.assertEqual(len(jm), 3, "number of lines in jarkfile")
-    def testExtractBaseUrls(self):
-        pass
-    
-class TldTest(unittest.TestCase):
-    def setUp(self):
-        pass
-    def testMakeTldUrl(self):
-        myurl = "http://whatever.com"
-        tld = "http://tldextract.appspot.com/api/extract?url="
-        self.assertEqual(tld + myurl, makeTldUrl(myurl), "tld url maker")
-    def testGetWebPage(self):
-        content = getWebPage("http://www.google.com")
-        self.assertTrue(len(content) > 5000, "page content")
-    def testParseTldResponse(self):
-        response = '{"domain": "abc", "subdomain": "", "tld": ""}'
-        parsed = parseTldResponse(response)
-        self.assertTrue(parsed.has_key("domain"))
-        self.assertTrue(parsed.has_key("subdomain"))
-        self.assertTrue(parsed.has_key("tld"))
-    def testSendTldUrls(self):
-        pass
-    def testBadTldUrl(self):
-        pass
-
-def runTestSuite():
-    suite1 = unittest.TestLoader().loadTestsFromTestCase(JarkfileTest)
-    suite2 = unittest.TestLoader().loadTestsFromTestCase(TldTest)
-    
-    mySuite = unittest.TestSuite([suite1, suite2])
-    unittest.TextTestRunner(verbosity=2).run(mySuite)
