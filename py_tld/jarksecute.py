710e278f334555c1bbbf23f951bc09b347dec75c
Jay Vyas Peerindex Dev Setup <jayunit100@gmail.com>
2012-02-21 22:06:35 -0500

    parameterized method for readJark file name

--- py_tld/jarksecute.py
+++ py_tld/jarksecute.py
@@ -5,18 +5,17 @@ LOG_FILENAME = "logging.txt"
 logging.basicConfig(filename = LOG_FILENAME, level = logging.DEBUG, filemode = 'w')
 
 
-def readJark():
+def readJark(filename):
     '''this is a junk function; it should be placed in a separate file'''
     #edit this variable to run on a real file
-    testpath = 'samplecsv.txt'
-    jarkModel = jk.parseJark(testpath)
+    jarkModel = jk.parseJark(filename)
     return jk.extractBaseUrls(jarkModel, 23)
 
 
 if __name__ == "__main__":
     # TODO: put this junk somewhere else
     logging.debug("reading jarkfile")
-    urls = readJark()
+    urls = readJark("samplecsv.txt")
     logging.debug("sending urls to tld extractor")
     jk.sendTldUrls(urls)
 else:
