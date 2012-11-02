65542bcc132c39f10e4e8e206a552b65601136d8
nsavageJVM <nigel.savage@rudolflabs.com>
2012-11-02 15:18:02 +0200

    AppTest.java

--- javamltb2/r1/src/test/java/jvm/AppTest.java
+++ javamltb2/r1/src/test/java/jvm/AppTest.java
@@ -7,8 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
     /**
      * Create the test case
@@ -33,6 +32,10 @@ public class AppTest
      */
     public void testApp()
     {
+
+        App.main(new String[] {"arg1"});
+    	
+    	
         assertTrue( true );
     }
 }
