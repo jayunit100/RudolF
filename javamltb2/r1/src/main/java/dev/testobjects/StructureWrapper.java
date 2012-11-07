59700814cfb97980e491fb6ee558e7cd565dd115
jayunit100 <jayunit100@gmail.com>
2012-11-07 17:50:16 -0500

    cleanup 5

--- javamltb2/r1/src/main/java/dev/testobjects/StructureWrapper.java
+++ javamltb2/r1/src/main/java/dev/testobjects/StructureWrapper.java
@@ -7,6 +7,11 @@ public class StructureWrapper {
 	 * @structurels are test objects for memory tests by sun.misc.Unsafe.class
 	 */
 	public class MyStructureEmpty { } // 8: 4 (start marker) + 4 (pointer to class)
-	public class MyStructureOneInt { public int x; } // 16: 4 (start marker) + 4 (pointer to class) + 4 (int) + 4 stuff bytes to align structure to 64-bit blocks
-	public class MyStructureTwoInt { int x; int y; } // 16: 4 (start marker) + 4 (pointer to class) + 2*4
+	public class MyStructureOneInt { 
+		public int x; 
+	}
+	// 16: 4 (start marker) + 4 (pointer to class) + 4 (int) + 4 stuff bytes to align structure to 64-bit blocks
+	public class MyStructureTwoInt { 
+		int x; int y; 
+	} // 16: 4 (start marker) + 4 (pointer to class) + 2*4
 }
