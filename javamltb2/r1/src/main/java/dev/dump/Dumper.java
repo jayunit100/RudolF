b5a460d9d9cd38463ed5a75d759318b0d92453db
nsavageJVM <nigel.savage@rudolflabs.com>
2012-11-09 22:23:31 +0200

    documentation complete for class Dumper.java

--- javamltb2/r1/src/main/java/dev/dump/Dumper.java
+++ javamltb2/r1/src/main/java/dev/dump/Dumper.java
@@ -4,21 +4,20 @@ import java.io.StringWriter;
 import java.util.concurrent.Callable;
 
 
-/*  This class dumps status on all threads at a specified time in the program 
- *    using  java.util.concurrent.Future
+/*  This class dumps status on all threads at the time specified time it is called
+ *  using  java.util.concurrent.Future
  * 
  *  @ runProcess the name of the process running in the jvm at the time of the dump
  * 	A daemon thread in Java is one that doesn't prevent the JVM from exiting
  * 
- * Methods
- * call:: this interface method aggregates all the system and instance threads to
- * construct a thread dump report 
+ * @ call() method of the  Callable in this class is implemented to aggregate
+ *   all the system and instance threads to construct a thread dump report 
  * 
- * dumpToDerby:: utility method, serializes data to embedded Derby database
+ * @ setReportLine() utility method, writes a line to the String output
  * 
- * dumpThreadGroupInfo:: aggregates information for child threads of the parent thread groups
+ * dumpThreadGroupInfo() aggregates information for child threads of the parent thread groups
  * 
- * this class can be called at any time in the program to get a snap shot of the running processes
+ * this class can be called at any time in the program to get a snap shot of the running threads
  * 
  */
 
@@ -33,30 +32,38 @@ public class Dumper implements Callable<String> {
 		this.runProcess = runProcess;
 		
 	}
-
+	
+	// java.util.concurrent.Callable. Callables unlike Runnables allow for return values.
+	// here we return a String which is a report of the data methods  called on all threads running 
+	// when this class is called 
 	@Override
 	public String call() {
 
 		// find the top thread group
 		ThreadGroup topThreadGroup = Thread.currentThread().getThreadGroup();
 
+		// climb up the thread hierarchy tree to the top, this is the parent thread group
 		while (topThreadGroup.getParent() != null)
 			topThreadGroup = topThreadGroup.getParent();
-
+		
+		// initial line for output identify the process this instance created for
 		sw.append("JVM Thread Info run for process " + runProcess + "\n");
 
+		// container for threads
 		Thread[] allThreads = new Thread[1000];
-
+		
+		// count the threads in the parent group
 		int threadCount = topThreadGroup.enumerate(allThreads, true);
+		
+		// initial line for output
+		sw.append("JVM Info:: Aggregrating Thread Group Data\n");
 
-		sw.append("JVM Info Aggregrating Thread Groups All Threads Report\n");
-
-		// dump info o all its threads
+		// dump info on all its threads
 		for (Thread th : allThreads) {
 
 			if (th == null)
 				continue;
-
+			// write data to a line in the report
 			setReportLine("Top Group Thread" + th.getName(),
 					(th.isDaemon() ? "daemon" : "not daemon "),
 					(th.isAlive() ? "alive" : "dead "), "to group NA",
@@ -64,31 +71,34 @@ public class Dumper implements Callable<String> {
 		}
 		
 		
-		// find all the child gtoups
+		// container for  all the child groups
 		ThreadGroup[] allGroups = new ThreadGroup[1000];
 
+		// count the threads in the the child groups
 		int numThreadGroups = topThreadGroup.enumerate(allGroups, true);
 
-		sw.append("JVM Info Number Thread Groups =" + numThreadGroups + "\n");
+		sw.append("JVM Info Number Child Thread Groups =" + numThreadGroups + "\n");
 
+		// if numThreadGroups == 1 them parent so we have data already
+		//if numThreadGroups > 1 child group so process for thread data
 		if (numThreadGroups > 1) {
 
 			for (ThreadGroup tg : allGroups) {
-				if (tg == null)
-					continue;
+				// sometimes find null threads so deal with this so loop does not crash with null pointer exception					
+				if (tg == null)	continue;
 
-				// dump info on all its threads
+				// dump info on all its threads in this group
 				dumpThreadGroupInfo(tg);
 				
 				}
 			}
 		
+		// return thread data report
 		return sw.toString();
-
 		
 	}
 	
-	
+	// a utility method to write a line in the output report String
 	void setReportLine(String name, String daemon, String alive, String destroyed, String Stack, String process, String group) {
 		
 		sw.append(name+"\n");
@@ -102,15 +112,16 @@ public class Dumper implements Callable<String> {
 	
 	// method to report info on child group threads
 	public void dumpThreadGroupInfo(ThreadGroup tg) {
-
+		// get the parent group name
 		String parentName = (tg.getParent() == null ? "No Parent" : tg.getName());
-
-		// Dump thread group info.
-
+		sw.append(parentName+"\n");
+		// container for  all the threads
 		Thread[] allThreads = new Thread[1000];
-
+		// count the threads
 		int threadCount = tg.enumerate(allThreads, false);
-
+		sw.append(parentName+" has "+threadCount+" threads"+"\n");
+		
+		// get the data for all the threads as a line in the report String
 		for (Thread th : allThreads) {
 			if (th == null)
 				continue;
