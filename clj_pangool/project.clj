95971ebb03c48d2e80493e802a29010d93415348
jayunit100 <jayunit100@gmail.com>
2012-11-09 17:29:48 -0500

    readme + lein proj

---
+++ clj_pangool/project.clj
@@ -0,0 +1,8 @@
+(defproject clj_pangool "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies 
+      [[org.clojure/clojure "1.4.0"]
+       [com.datasalt.pangool/pangool-core "0.40"]])
