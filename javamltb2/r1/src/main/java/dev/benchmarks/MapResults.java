76bdc227b7170abdd4890562114afdd3f3daf374
nsavageJVM <nigel.savage@rudolflabs.com>
2012-11-09 16:34:22 +0200

    include thread dump and improve concurrency

--- javamltb2/r1/src/main/java/dev/benchmarks/MapResults.java
+++ javamltb2/r1/src/main/java/dev/benchmarks/MapResults.java
@@ -5,6 +5,12 @@ import java.util.Hashtable;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import dev.dump.Dumper;
 
 /*   This class obtains the Map Benchmark Results
  * 
@@ -22,29 +28,48 @@ import java.util.concurrent.ConcurrentHashMap;
 
 
 
-public class MapResults implements Callable<HashMap<String, HashMap<String, Long>>>  {
+public class MapResults implements Callable<HashMap<String, HashMap<String, String>>>  {
 	
+	private final  ExecutorService threadExecutor = Executors.newCachedThreadPool();
+	private final HashMap<String, HashMap<String, String>> resultsMap = new HashMap<String, HashMap<String, String>>();
+	private final HashMap<String, String> dumpResults = new  HashMap<String, String>();
+	private final Dumper dumpTask;
+	private  Future<String> mapDumpTask;
 
-	final HashMap<String, HashMap<String, Long>> resultsMap = new HashMap<String, HashMap<String, Long>>();
 
-	
-	
+
+	public MapResults(final Dumper dumpTask) {
+		super();
+		this.dumpTask = dumpTask;
+	}
+
+
 	@Override
-	public HashMap<String, HashMap<String, Long>> call() {
+	public HashMap<String, HashMap<String, String>> call() {
 		
-		resultsMap.put("hashMap", hashMapResult() );
+		resultsMap.put("hashMap", hashMapResult());
 		resultsMap.put("concurrentMap", concurrentHashMapResult() );
 		resultsMap.put("hashTable", hashtableResult());
-
+		
+		try {
+			dumpResults.put("single" ,mapDumpTask.get());
+			
+		} catch (InterruptedException e) {e.printStackTrace();
+		} catch (ExecutionException e) {e.printStackTrace();
+		}
+		resultsMap.put("dumpReport", dumpResults);
+		threadExecutor.shutdown();
 		return resultsMap;
 		}
 
 	
-	public HashMap<String, Long> hashMapResult() {
-	final HashMap<String, Long> results = new HashMap<String, Long>();
+	public HashMap<String, String> hashMapResult() {
+	final HashMap<String, String> results = new HashMap<String,String>();
   	final Map<Object,Object> testMap = new HashMap<Object, Object>();// unsynched map
   	long mem = 0;
   	int k =1;
+  	
+  	mapDumpTask = threadExecutor.submit(dumpTask);
   	
   	  for(int i = 0; i< 100; i++) {
 
@@ -53,8 +78,9 @@ public class MapResults implements Callable<HashMap<String, HashMap<String, Long
 	            mem =  mem +  displayMemory();
 
 	            }
-			 	results.put("memStat",mem/k);	
-			 	results.put("instStat",(long) k);
+  	  			mem =mem/k;
+			 	results.put("memStat",""+mem/k);	
+			 	results.put("instStat",""+ k);
 	
 		 
 	  return results;	
@@ -62,8 +88,8 @@ public class MapResults implements Callable<HashMap<String, HashMap<String, Long
 			}
 	
 	
-	public HashMap<String, Long> concurrentHashMapResult() {
-	final HashMap<String, Long> results = new HashMap<String, Long>();
+	public HashMap<String, String> concurrentHashMapResult() {
+	final HashMap<String, String> results = new HashMap<String, String>();
 	final Map<Object,Object> testSync = new ConcurrentHashMap<Object,Object>();
   	long mem = 0;
   	int k =1;
@@ -75,8 +101,9 @@ public class MapResults implements Callable<HashMap<String, HashMap<String, Long
 	            mem =  mem +  displayMemory();
 
 	            }
-			 	results.put("memStat",mem/k);	
-			 	results.put("instStat",(long) k);
+		mem =mem/k;
+	 	results.put("memStat",""+mem/k);	
+	 	results.put("instStat",""+ k);
 
 		 
 	  return results;	
@@ -84,8 +111,8 @@ public class MapResults implements Callable<HashMap<String, HashMap<String, Long
 			}
 	 
 	
-		public HashMap<String, Long> hashtableResult() {
-			final HashMap<String, Long> results = new HashMap<String, Long>();
+		public HashMap<String,String> hashtableResult() {
+			final HashMap<String, String> results = new HashMap<String, String>();
 			 final Hashtable<Object,Object> table = new Hashtable<Object, Object>();
 		  	long mem = 0;
 		  	int k =1;
@@ -97,8 +124,9 @@ public class MapResults implements Callable<HashMap<String, HashMap<String, Long
 			            mem =  mem +  displayMemory();
 
 			            }
-					 	results.put("memStat",mem/k);	
-					 	results.put("instStat",(long) k);
+				mem =mem/k;
+			 	results.put("memStat",""+mem/k);	
+			 	results.put("instStat",""+ k);
 
 				 
 			  return results;	
@@ -112,5 +140,7 @@ public class MapResults implements Callable<HashMap<String, HashMap<String, Long
 			    r.gc(); // gc sweep one is not enough
 			   return r.totalMemory()-r.freeMemory();
 			}
+			
 
+			
 }
