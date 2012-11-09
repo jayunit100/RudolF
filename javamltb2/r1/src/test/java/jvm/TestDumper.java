4d9743cac3c965e7bb3114e8da5f42d449ca921c
nsavageJVM <nigel.savage@rudolflabs.com>
2012-11-09 16:36:24 +0200

    implement staged tests

--- javamltb2/r1/src/test/java/jvm/TestDumper.java
+++ javamltb2/r1/src/test/java/jvm/TestDumper.java
@@ -2,10 +2,22 @@ package jvm;
 
 import org.junit.Test;
 
+import dev.JvmResearchTest;
+
 public class TestDumper {
 
 	@Test
-	public void test1(){
+	public void test1()  throws Exception {
 		
+		JvmResearchTest.stageOneTest();
 	}
+	
+	
+	@Test
+	public void test2()  throws Exception {
+		
+		JvmResearchTest.stageTwoTest();
+	}
+	
+	
 }
