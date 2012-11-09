0767e045c6625ec7982dc5e6207ff7ac91ada41f
nsavageJVM <nigel.savage@rudolflabs.com>
2012-11-09 16:35:46 +0200

    implement staged tests

--- javamltb2/r1/src/test/java/dev/JvmResearchTest.java
+++ javamltb2/r1/src/test/java/dev/JvmResearchTest.java
@@ -1,76 +1,62 @@
 package dev;
 
-import java.io.File;
-import java.util.Arrays;
-import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import dev.derbyutils.DerbyTask;
-import dev.derbyutils.DerbyUtils;
-import dev.testobjects.StructureWrapper;
+import java.util.concurrent.Future;
+import dev.benchmarks.BenchMarkRunner;
+import dev.dump.Dumper;
 
 /* 
  * ideas
  * 1 set up a resource intensive process to test JVM
- * 2 investigate Direct Memory Access (DMA) in Java
- * derby is a pure java embedded object that can be  target of profiling and obvious store bench mark results
- * 
+
  * main loop  to be investigated for the stack memory and loop optimization
- * 
- * Franken class is sun.misc.Unsafe.class wrapper for objects to be investigated and manipulated with DMA
- * 
- * struct list is just a wrapper for some structures for testing Unsafe
+
  */
 public class JvmResearchTest {
 
-	// initial testing for Unsafe behaviour
-	private static final List<Object> structList;
+	
 
-	// static initialiser
-	static {
-		structList = Arrays.asList(
-				new StructureWrapper().new MyStructureEmpty(),
-				new StructureWrapper().new MyStructureOneInt(),
-				new StructureWrapper().new MyStructureTwoInt());
-	}
-	// for derby sys independent path
-	private static String sep = File.separator;
-	// derby object id
-	private static Integer k;
+	public static void stageOneTest() throws InterruptedException, ExecutionException {
 
-	public static void main(String[] args) throws Exception {
-
-		/* results database */
-		DerbyUtils.makeDerby("benchMarkResults");
-		// thread container
+		// java concurrency thread container
 		ExecutorService threadExecutor = Executors.newCachedThreadPool();
+		
+		// java concurrency Future task to obtain a thread dump
+		Future<String> dumpTask;
+		dumpTask = threadExecutor.submit(new Dumper("Initial Thread state task " ));
+		System.out.println("Running Test 1 Initial Thread Dump:"+dumpTask.get());
 
-		k = 1;// count the loops if needed
-		try {
-			for (Object obj : structList) {
-				// pass thread an object to be investigated or manipulated
-				DerbyTask derbyTask = new DerbyTask(obj, k);
-				// pause for a while as derby is slow
-				threadExecutor.execute(derbyTask);
+		dumpTask = threadExecutor.submit(new Dumper("Initial Thread state task " ));
+				
+		System.out.println("Initial Thread dump task result:"+dumpTask.get());		
 
-				try {
-					Thread.sleep(400);
-				} catch (InterruptedException e) {
-					// do nothing
-				}
-				System.out.println("insert: " + (k++) + ", completed");
+			// all done drop the database as is just system  test data for now
+		threadExecutor.shutdown();
 
-			}// end loop
+			}
+	
+	public static void stageTwoTest() throws InterruptedException, ExecutionException {
 
-		} finally {
+		// java concurrency thread container
+		ExecutorService threadExecutor = Executors.newCachedThreadPool();
+		Future<String> benchMarkTask;
+		System.out.println("Please wait while running Java Collection Benchmarks may take a minute");
+		// run collection benchmarks
+		benchMarkTask = threadExecutor.submit(new BenchMarkRunner());
+		System.out.println(benchMarkTask.get());
+		System.out.println("");
+		System.out.println("End  Java Collection Benchmarks");
+		System.out.println("");
+	
+			// all done drop the database as is just system  test data for now
+		threadExecutor.shutdown();
 
-			// all done drop the database as is just junk test data for now
-			System.out.println("report Derby");
-			System.out.println("dropDerby");
-			threadExecutor.shutdown();
-
-			DerbyUtils.dropDerby("benchMarkResults");
-		}
-
+			}
+	
+	
+	
+	
 	}
-}
+
