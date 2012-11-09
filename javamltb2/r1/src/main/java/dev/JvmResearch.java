94952ecdce369a65e749283d21d73ddc97496e41
nsavageJVM <nigel.savage@rudolflabs.com>
2012-11-09 16:31:41 +0200

    remove derby and clean

--- javamltb2/r1/src/main/java/dev/JvmResearch.java
+++ javamltb2/r1/src/main/java/dev/JvmResearch.java
@@ -8,20 +8,12 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import dev.benchmarks.BenchMarkRunner;
-import dev.derbyutils.DerbyTask;
-import dev.derbyutils.DerbyUtils;
 import dev.dump.Dumper;
-import dev.testobjects.StructureWrapper;
 
 /**  
  *  This class is the entry point for the benchmarking and optimization code
  * 	results will be stored in an embedded Derby database.
  * 
- *  @ structList an array of simple structures use for memory tests by sun.misc.Unsafe.class
- * 
- *  @ Franken class is a wrapper class to obtain an instance of sun.misc.Unsafe.class 
- *  
- *  @ DerbyTask is a wrapper class to obtain an instance of sun.misc.Unsafe.class 
  * 
  *  @ dumpTask class dumps status on all threads at a specified time in the program 
  *    using  java.util.concurrent.Future
@@ -33,65 +25,35 @@ import dev.testobjects.StructureWrapper;
  */
 public class JvmResearch {
 
-	// initial testing for Unsafe behaviour
-	private static final List<Object> structList;
-
-	// static initialiser
-	static {
-		structList = Arrays.asList(
-				new StructureWrapper().new MyStructureEmpty(),
-				new StructureWrapper().new MyStructureOneInt(),
-				new StructureWrapper().new MyStructureTwoInt());
-	}
-	// for derby sys independent path
-	private static String sep = File.separator;
-	// derby object id
-	private static int k;
 
 	public static void main(String[] args) throws Exception {
 
-		/* results database */
-		DerbyUtils.makeDerby("benchMarkResults");
-		// thread container
+
+		// java concurrency thread container
 		ExecutorService threadExecutor = Executors.newCachedThreadPool();
-		Future<String> dumpTask;
-
-		k = 1;// count the loops if needed and unique id for derby entries
-		try {
 		
-			System.out.println("Starting...");
-			for (Object obj : structList) {
-				System.out.println("Object:"+obj);
-				// pass thread an object to be investigated or manipulated
-				DerbyTask derbyTask = new DerbyTask(obj, k);
-				// pause for a while as derby is slow
-				threadExecutor.execute(derbyTask);
+		// java concurrency Future task to obtain a thread dump
+		Future<String> dumpTask;
+		
+		//  java concurrency Future task to run collection benchmarks
+		Future<String> benchMarkTask;
+		
+		// get initial thread information
+		dumpTask = threadExecutor.submit(new Dumper("Initial Thread state task " ));
 				
-				dumpTask = threadExecutor.submit(new Dumper("structList memory task "+k, k ));
-				
-				System.out.println("result:"+dumpTask.get());
-
-				try {
-					Thread.sleep(800);
-				} catch (InterruptedException e) {
-				}
-				;
-				System.out.println("insert: " + (k++) + ", completed");
-
-			}// end loop
-			System.out.println("");
-			System.out.println("Please wait while running Java Collection Benchmarks may take a minute");
+		System.out.println("Initial Thread dunp task result:"+dumpTask.get());
+	
+		System.out.println("");
+		System.out.println("Please wait while running Java Collection Benchmarks may take a minute");
 			// run collection benchmarks
-			BenchMarkRunner.getResults();
-			System.out.println("");
-			System.out.println("End  Java Collection Benchmarks");
-			System.out.println("");
+		benchMarkTask = threadExecutor.submit(new BenchMarkRunner());
+		System.out.println(benchMarkTask.get());
+		System.out.println("");
+		System.out.println("End  Java Collection Benchmarks");
+		System.out.println("");
 
-		} 
-		finally {
 			// all done drop the database as is just system  test data for now
-			threadExecutor.shutdown();
-			DerbyUtils.dropDerby("benchMarkResults");
-		}
+		threadExecutor.shutdown();
+
 	}
 }
