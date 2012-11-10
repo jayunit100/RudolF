db1c0232bb9b3654410a23e5cd681c1619049846
nsavageJVM <nigel.savage@rudolflabs.com>
2012-11-10 16:09:43 +0200

    documentation updated

--- javamltb2/r1/src/main/java/dev/JvmResearch.java
+++ javamltb2/r1/src/main/java/dev/JvmResearch.java
@@ -9,29 +9,34 @@ import dev.dump.Dumper;
 
 /**  
  *  This class is the entry point for the benchmarking and thread dump code
- *  reports results as a simple screen dump
+ *  
+ *  This class aggregates data(Strings) from dev.benchmarks.BenchMarkRunner and dev.dump.Dumper
+ *  
+ *  it reports the results (Strings) for both the thread dump and benchmarks as a simple screen dump
  *  
  *  @ dumpTask class dumps status on all threads at a specified time in the program 
- *    using  java.util.concurrent.Future
+ *    using  java.util.concurrent.Future. Using the "java.util.concurrent.Executors"
+ *    interfaces Future and Callable the thread dump can be called at any time in the program
  * 
- *  @ benchMarkTask  Bench Marks for Java Collection Data Structures
- *  
- * This class aggregates data from dev.benchmarks.BenchMarkRunner and dev.dump.Dumper
+ *  @ benchMarkTask  Bench Marks for Java Collection Data Structures 
  * 
- * String from dev.benchmarks.BenchMarkRunner shows memory footprint for concurrent implementations of 
+ * The Strings from dev.benchmarks.BenchMarkRunner show the  memory footprint for implementations of 
  * java.util.concurrent.ConcurrentHashMap and java.util.concurrent.CopyOnWriteArrayList compared to the 
  * standard maps and lists in java.util.*
  * 
- * From the JavaDoc CopyOnWriteArrayList is  a "thread-safe variant of ArrayList in which all mutative 
+ * From the JavaDoc for "java.util.concurrent" 
+ * 
+ * "CopyOnWriteArrayList is  a "thread-safe variant of ArrayList in which all mutative 
  * operations (add, set, and so on) are implemented by making a fresh copy of the array." 
  * The collection internally copies its contents over to a new array upon any modification, so readers 
- * accessing the contents of the array incur no synchronization costs (because they're never operating on mutable data).
+ * accessing the contents of the array incur no synchronization costs (because they're never operating on mutable data)."
  * 
  * The List BenchMarks compare the cost of implementing CopyOnWriteArrayList to Collections.synchronizedList
  * 
  * Map hosts a subtle concurrency bug. When a Map is accessed from multiple threads, the use of either containsKey() or get() 
  * to find out whether a given key is present before storing the key/value pair allows for another thread 
- * to seize control of the Map. The  lock is acquired at the start of get(), then released before the lock can be acquired again, in the call to put(). 
+ * to seize control of the Map. The  lock is acquired at the start of get(), then released before the lock can be acquired 
+ * again, in the call to put(). 
  * The result is a race condition between the two threads, the outcome will be different based on which runs first. Worse 
  * if two threads call a method at concurrently, each will test and put, losing the first thread's value in the process
  * 
