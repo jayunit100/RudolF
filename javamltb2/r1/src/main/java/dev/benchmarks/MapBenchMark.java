85ba840bb5ea54f47cd68e4f2607d8c1658554e8
nsavageJVM <nigel.savage@rudolflabs.com>
2012-11-13 22:03:29 +0200

    BenchMark Maps for new design @Jay

--- javamltb2/r1/src/main/java/dev/benchmarks/MapBenchMark.java
+++ javamltb2/r1/src/main/java/dev/benchmarks/MapBenchMark.java
@@ -1,9 +1,53 @@
 package dev.benchmarks;
 
-public class MapBenchMark {
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MapBenchMark extends BenchMarkBase {
 	
-	
-	
+	/**   This class obtains the  new Benchmark results for the java.util.concurrent.ConcurrentHashMap 
+	 *   compared to the standard Maps found in the  java.util.* Collections package. 
+	 */
+
 	
 
+	public void getMapResults() {
+		
+		start=System.currentTimeMillis();
+		
+		Map hashMap;
+		
+		Hashtable  hashTable;
+		
+		Map syncMap;	
+		
+		for(int i = 0 ; i < 10 ; i++){
+			long start=0L;
+			
+			start=System.currentTimeMillis();
+			hashMap =  new HashMap();
+			populateMap(hashMap);
+			System.out.println("HashMap "+(System.currentTimeMillis()-start));
+			
+			start=System.currentTimeMillis();
+			syncMap = new ConcurrentHashMap();
+			populateMap(syncMap);
+			System.out.println("ConcurrentHashMap "+(System.currentTimeMillis()-start));
+			
+			
+			
+			start=System.currentTimeMillis();
+			hashTable = new Hashtable();
+			populateMap(hashTable);
+			System.out.println("Hashtable "+(System.currentTimeMillis()-start));
+		
+		
+			}
+	
+	
+	}
+	
+	
 }
