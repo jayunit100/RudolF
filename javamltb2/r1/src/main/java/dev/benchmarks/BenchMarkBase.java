81beb214d2e781cca0d12a506a8dfb963cd32d0a
nsavageJVM <nigel.savage@rudolflabs.com>
2012-11-13 15:46:02 +0200

    bench mark supoer class

---
+++ javamltb2/r1/src/main/java/dev/benchmarks/BenchMarkBase.java
@@ -0,0 +1,29 @@
+package dev.benchmarks;
+
+/**  This class is the SuperClass point for the Bench Mark Classes, it obtains a report String
+ *  from the bench mark classes dev.benchmarks.MapResults and dev.benchmarks.ListResults, 
+ *  it then passes this along to the main class dev.JvmResearch.java  for reporting as a 
+ *  simple screen dump. During the benchmarking we poll the thread groups for thread 
+ *  data and aggregate this into the bench mark report.
+ *  
+ *  From this class we can measure the memory cost of the java.util.concurrent  Collections
+ *  CopyOnWriteArrayList and ConcurrentMap compared to the standard Collections classes
+ * 
+ *  From this class we can gain understanding of concurrency and its implications for the Java Memory Model
+ *  
+ *  @ mapTask is  benchmark thread for concurrent map
+ * 
+ *
+ * 
+ */
+
+
+
+public class BenchMarkBase {
+	
+	
+	
+	
+	
+
+}
