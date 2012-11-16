a1523f5fb3477ac1f66eb4d75bc441be786f4f47
nsavageJVM <nigel.savage@rudolflabs.com>
2012-11-16 14:58:48 +0200

    remove old test code

--- javamltb2/r1/src/test/java/dev/JvmResearchTest.java
+++ javamltb2/r1/src/test/java/dev/JvmResearchTest.java
@@ -4,7 +4,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import dev.benchmarks.BenchMarkRunner;
 import dev.benchmarks.ListBenchMark;
 import dev.benchmarks.MapBenchMark;
 import dev.dump.Dumper;
@@ -18,46 +17,7 @@ import dev.dump.Dumper;
 public class JvmResearchTest {
 
 	
-// for now this just aggregates the threads while the system is idle
-	public static void stageOneTest() throws InterruptedException, ExecutionException {
 
-		// java concurrency thread container
-		ExecutorService threadExecutor = Executors.newCachedThreadPool();
-		
-		// java concurrency Future task to obtain a thread dump
-		Future<String> dumpTask;
-		dumpTask = threadExecutor.submit(new Dumper("Initial Thread state task " ));
-		System.out.println("Running Test 1 Initial Thread Dump for idle system:");
-
-		dumpTask = threadExecutor.submit(new Dumper("Initial Thread state task " ));
-				
-		System.out.println("Initial Thread dump task result:"+"\n"+dumpTask.get());		
-		System.out.println("End Test 1 Initial Thread Dump for idle system");
-		System.out.println("End Test 1 "+"\n");
-		// all done drop the database as is just system  test data for now
-		threadExecutor.shutdown();
-
-			}
-	
-	public static void stageTwoTest() throws InterruptedException, ExecutionException {
-
-		// java concurrency thread container
-		ExecutorService threadExecutor = Executors.newCachedThreadPool();
-		Future<String> benchMarkTask;
-		System.out.println("Running Test 2 Java Collection Benchmarks with Thread Dump:"+"\n");
-		System.out.println("Please wait while running Java Collection Benchmarks may take a minute"+"\n");
-		// run collection benchmarks
-		benchMarkTask = threadExecutor.submit(new BenchMarkRunner());	
-		System.out.println(benchMarkTask.get());
-		System.out.println("");
-		System.out.println("End  Java Collection Benchmarks with Thread Dump");
-		System.out.println("");
-		System.out.println("End Test 2 "+"\n");
-		System.out.println("End Tests "+"\n");
-			// all done drop the database as is just system  test data for now
-		threadExecutor.shutdown();
-
-			}
 	
 	public static void stageOneTestNewCode() throws InterruptedException, ExecutionException {
 
