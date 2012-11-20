b4321469ec09835ef2a9ebb152856c7b51de839d
jayunit100 <jayunit100@gmail.com>
2012-11-20 14:24:37 -0500

    magnitude order of precision in example 3. blog post forthcoming

--- javamltb2/r1/src/main/java/net/rudolfcode/jvm/Example3.java
+++ javamltb2/r1/src/main/java/net/rudolfcode/jvm/Example3.java
@@ -9,21 +9,32 @@ import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Multimap;
 
+/**
+ * Is a vector really slower than an arraylist? YES.
+ * How much more precise is CPU time measurement than time measurement? ~ ONE ORDER OF MAGNITUDE.
+ */
 public class Example3 {
 
-	
 	public static void main(String[] args) throws Exception{
-
-		long numThreads=ManagementFactory.getThreadMXBean().getTotalStartedThreadCount();
 		Multimap<String,Long> classes = ArrayListMultimap.create();
 		ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
 		for(Collection c : new Collection[]{new Vector(), new ArrayList(), new Vector(), new ArrayList(), new Vector(), new ArrayList()} ){
-			Thread t1 = new ProfileThread(c);
-			long cpuStart1=ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
-			t1.start();
-			t1.join();
-			long cpu=(ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime()-cpuStart1);
-			classes.put(c.getClass().getName(),cpu);
+			{
+				Thread t1 = new ProfileThread(c.getClass().newInstance());
+				long cpuStart1=ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
+				t1.start();
+				t1.join();
+				long cpu1=(ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime()-cpuStart1);
+				classes.put(c.getClass().getName()+"_cpu_time",cpu1);
+			}
+			{
+				Thread t2 = new ProfileThread(c.getClass().newInstance());
+				long cpuStart2=ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
+				t2.start();
+				t2.join();
+				long cpu2=(ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime()-cpuStart2);
+				classes.put(c.getClass().getName()+"_total_time",cpu2);
+			}
 		}
 		System.out.println(classes);
 	}
