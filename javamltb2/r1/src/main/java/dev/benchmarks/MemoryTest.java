499006f56474ed824f0ebc54f435e44e719e35e7
jayunit100 <jayunit100@gmail.com>
2012-11-20 10:50:42 -0500

    memory test format

--- javamltb2/r1/src/main/java/dev/benchmarks/MemoryTest.java
+++ javamltb2/r1/src/main/java/dev/benchmarks/MemoryTest.java
@@ -18,44 +18,50 @@ public class MemoryTest extends BenchMarkBase {
 	
      
 	// later this method can be expanded to deal with arbitary types passed in by class name 
-	public void genericCollectionAnalyzer() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-	// initila values for pre and post object creation 	
-	long usedMemoryXBean = 0;
-	long usedMemoryRunTime = 0;
-	long initialMemoryXBean = 0;
-	long initialMemoryRunTime = 0;
-	
-	Object[] objects = new Object[100];
-	long size= 0;
-	
-	// we do this because later this method can be extended to analyze class types passed in as a parameter
-	Class clazz = Class.forName ("java.util.Hashtable");
-	
-	// get the initial memory using runtime and xbeans
-	initialMemoryXBean = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed();
-	initialMemoryRunTime = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-	
-	
-	// Create a primer instance of the class before your measure the heap the first time
-	Object primer = clazz.newInstance();
-	 for (int i = 0; i < objects.length; i++) {
-         objects[i] = clazz.newInstance();
-      }
-	// get the final  memory using runtime and xbeans
-	 usedMemoryXBean = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed();
-	 usedMemoryRunTime = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory(); 
-	 
-	 
-	 // simple routine to calculate an average size for 100 objects
-	 float approxSizeXBean = (usedMemoryXBean -   initialMemoryXBean) / 100f;
-	 approxSizeXBean = Math.round(approxSizeXBean);
-	// simple routine to calculate an average size for 100 objects
-	 float approxSizeRunTime = (usedMemoryRunTime -   initialMemoryRunTime) / 100f;
-	 approxSizeRunTime = Math.round(approxSizeRunTime);
-	 
-	 System.out.println("size of empty hashtable by XBean = "+approxSizeXBean+" size of empty hashtable by RunTime = "+ approxSizeRunTime );
-	 
-	 
+	public void genericCollectionAnalyzer() throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException {
+		// initila values for pre and post object creation
+		long usedMemoryXBean = 0;
+		long usedMemoryRunTime = 0;
+		long initialMemoryXBean = 0;
+		long initialMemoryRunTime = 0;
+
+		Object[] objects = new Object[100];
+		long size = 0;
+
+		// we do this because later this method can be extended to analyze class
+		// types passed in as a parameter
+		Class clazz = Class.forName("java.util.Hashtable");
+
+		// get the initial memory using runtime and xbeans
+		initialMemoryXBean = ManagementFactory.getMemoryMXBean()
+				.getHeapMemoryUsage().getUsed();
+		initialMemoryRunTime = Runtime.getRuntime().totalMemory()
+				- Runtime.getRuntime().freeMemory();
+
+		// Create a primer instance of the class before your measure the heap
+		// the first time
+		Object primer = clazz.newInstance();
+		for (int i = 0; i < objects.length; i++) {
+			objects[i] = clazz.newInstance();
+		}
+		// get the final memory using runtime and xbeans
+		usedMemoryXBean = ManagementFactory.getMemoryMXBean()
+				.getHeapMemoryUsage().getUsed();
+		usedMemoryRunTime = Runtime.getRuntime().totalMemory()
+				- Runtime.getRuntime().freeMemory();
+
+		// simple routine to calculate an average size for 100 objects
+		float approxSizeXBean = (usedMemoryXBean - initialMemoryXBean) / 100f;
+		approxSizeXBean = Math.round(approxSizeXBean);
+		// simple routine to calculate an average size for 100 objects
+		float approxSizeRunTime = (usedMemoryRunTime - initialMemoryRunTime) / 100f;
+		approxSizeRunTime = Math.round(approxSizeRunTime);
+
+		System.out.println("size of empty hashtable by XBean = "
+				+ approxSizeXBean + " size of empty hashtable by RunTime = "
+				+ approxSizeRunTime);
+
 	}
 	
 	
