e643b9a58c033c7d9bbd5fa9ad494d2623a0861b
jayunit100 <jayunit100@gmail.com>
2012-11-21 15:50:06 -0500

    update example 3A that shows how nanotime and mxbean work

--- javamltb2/r1/src/main/java/net/rudolfcode/jvm/Example3.java
+++ javamltb2/r1/src/main/java/net/rudolfcode/jvm/Example3.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.Vector;
 
 import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Multimap;
 
 /**
@@ -16,42 +15,55 @@ import com.google.common.collect.Multimap;
 public class Example3 {
 
 	public static void main(String[] args) throws Exception{
+		for(float i = 4.0f ; i < 7.4 ; i+=.3){
+			profile((int)Math.pow(10, i));
+		}
+	}
+
+	private static void profile(int nums) {
 		Multimap<String,Long> classes = ArrayListMultimap.create();
 		ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
-		for(Collection c : new Collection[]{new Vector(), new ArrayList(), new Vector(), new ArrayList(), new Vector(), new ArrayList()} ){
+		for(Collection c : new Collection[]{new Vector(), new ArrayList()}){
 			{
-				Thread t1 = new ProfileThread(c.getClass().newInstance());
 				long cpuStart1=ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
-				t1.start();
-				t1.join();
+				run(c,nums);
 				long cpu1=(ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime()-cpuStart1);
 				classes.put(c.getClass().getName()+"_cpu_time",cpu1);
 			}
 			{
-				Thread t2 = new ProfileThread(c.getClass().newInstance());
-				long cpuStart2=ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
-				t2.start();
-				t2.join();
-				long cpu2=(ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime()-cpuStart2);
+				long cpuStart2=System.currentTimeMillis();
+				run(c,nums);
+				long cpu2=(System.currentTimeMillis()-cpuStart2);
 				classes.put(c.getClass().getName()+"_total_time",cpu2);
 			}
 		}
-		System.out.println(classes);
+		
+		//the "cpu" time : pure measurement of time the cpu is spent during execution.
+		Collection<Long> listtime=classes.get("java.util.ArrayList_cpu_time");
+		Collection<Long> vectime=classes.get("java.util.Vector_cpu_time");
+		
+		//the "real world" time : susceptible to fluctuations due to memory and system load.
+		Collection<Long> listtimes=classes.get("java.util.ArrayList_total_time");
+		Collection<Long> vectimes=classes.get("java.util.Vector_total_time");
+		System.out.println(nums + "," + average(listtime)+","+average(vectime)+","+average(listtimes)+","+average(vectimes));
 	}
 	
-	public static class ProfileThread extends Thread{
-			
-			Collection impl;
-			public ProfileThread(Collection c){
-				impl=c;
-			}
-		
-			@Override
-			public void run() {
-				impl.clear();
-				for(int x = 10 ; x < 1000000; x++){
-					impl.add(x);
-				}
-			}
+	public static float average(Collection<Long> l){
+		long value=0;
+		for(Long v : l){
+			value+=v;
+		}
+		return ((float)value)/(float)l.size();
 	}
+		
+	/**
+	 * Add "size" elements into a collection .
+	 */
+	public static void run(Collection impl, int size) {
+		impl.clear();
+		for(int x = 10 ; x < size; x++){
+			impl.add((short)x);
+		}
+	}
+
 }
